7a7ca55aa8b4166b4aa3d12274b8d87ee015338d
andy.patterson <andy.patterson@genesys.com>
2018-03-14 15:46:45 -0400

    add unit test to map showing different types and an additional function

--- test/map.js
+++ test/map.js
@@ -5,17 +5,27 @@ const add1 = (x) => {
     return x + 1;
 };
 
+const parseNum = (x) => {
+    return x + '1';
+};
+
 describe('map', () => {
+    it('should return an empty array if given an empty array', () => {
+        const result = map([], add1);
+
+        assert.arrayEqual(result, []);
+    });
+
     it('should return an array containing output of function across each element', () => {
         const result = map([1, 2, 3, 4], add1);
 
         assert.arrayEqual(result, [2, 3, 4, 5]);
     });
 
-    it('should return an empty array if given an empty array', () => {
-        const result = map([], add1);
+    it('should work with any type of array', () => {
+        const result = map(['1', '2', '3', '4'], parseNum);
 
-        assert.arrayEqual(result, []);
+        assert.arrayEqual(result, [1, 2, 3, 4]);
     });
 });
 
